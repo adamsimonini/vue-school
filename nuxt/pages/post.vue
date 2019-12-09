27a3b1a0a3357f8e7ae66b241cf10f3f985858a2
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-12-09 16:54:35 -0500

    Started Nuxt Lessons

---
+++ nuxt/pages/post.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+  <article>
+    <h1 class="title">This is a title</h1>
+  </article>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
